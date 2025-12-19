ecb71b8020e1f79cadf3183fbe24ab45e2f59a94
kunukulavanya03 <kunukulavanya03@gmail.com>
2025-12-19 11:39:05 +0530

    🚀 Initial commit - Generated with CODE AGENT

--- frontend/src/components/ui/checkbox.tsx
+++ frontend/src/components/ui/checkbox.tsx
@@ -1,11 +1,8 @@
 "use client";
-
 import * as React from "react";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox@1.1.4";
 import { CheckIcon } from "lucide-react@0.487.0";
-
 import { cn } from "./utils";
-
 function Checkbox({
   className,
   ...props
@@ -28,5 +25,4 @@ function Checkbox({
     </CheckboxPrimitive.Root>
   );
 }
-
 export { Checkbox };
